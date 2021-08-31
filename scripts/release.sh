6804af7297ea7261700899281451e7b71d010232
Alexander Rogalskiy <arogalskii@sbereducation.ru>
2021-08-31 09:38:46 +0300

    Updates on files
    Added changeset on workflow

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# build images & push to hub
+set -e
 
+cd "$(dirname "$0")" || exit 1
+
+# build images & push to hub
 TAG=$(node -p "require('./package.json').version")
-IMAGE="sensiblemetrics/styled-wotd"
+IMAGE="io.sensiblemetrics/styled-wotd"
 
 echo "Building $IMAGE:$TAG"
 
